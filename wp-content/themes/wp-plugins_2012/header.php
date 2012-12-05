5616dbcf32ebcd575176c429d4bbc9b0ec473f46
Adriano Ribeiro <adriano.ribeiro@gmail.com>
2012-12-05 18:10:41 -0200

    Inclusão da badge do Github

--- wp-content/themes/wp-plugins_2012/header.php
+++ wp-content/themes/wp-plugins_2012/header.php
@@ -54,9 +54,15 @@
 				$('.github-widget').githubWidget();
 			</script>
 
+
+
 		</hgroup>
 
 	</header>
+
+  <a href="https://github.com/a2comunicacao/wp-plugins">
+    <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub">
+  </a>
 
 <!-- Begin Page -->
 <div class="container">
