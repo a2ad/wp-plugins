3be1091592a0aa4f6d42e20c3a3d2893a5283249
Adriano Ribeiro <adriano.ribeiro@gmail.com>
2012-12-05 15:51:27 -0200

    Novo layout de 3 colunas para a homepage

--- wp-content/themes/wp-plugins_2012/index.php
+++ wp-content/themes/wp-plugins_2012/index.php
@@ -12,37 +12,37 @@
 get_header(); ?>
 
     
-    <?php
-            // get all the categories from the database
-            $cats = get_categories(); 
+<?php
+    // get all the categories from the database
+    $cats = get_categories(); 
+    echo "<div class='twelve plugins-list'>";
+    // loop through the categries
+    foreach ($cats as $cat) {
 
-            // loop through the categries
-            foreach ($cats as $cat) {
-                echo "<div class='six columns'>";
-                // setup the cateogory ID
-                $cat_id= $cat->term_id;
-                // Make a header for the cateogry
-                echo "<h2>".$cat->name."</h2>";
-                // create a custom wordpress query
-                query_posts("cat=$cat_id&post_per_page=100");
-        ?>
-            <ul class="twelve side-nav">
-        <?php
-                // start the wordpress loop!
-        if (have_posts()) : while (have_posts()) : the_post();
-        ?>
-                
-                <li><a href="<?php the_permalink();?>"><?php the_title(); ?></a></li>
-                <li class="divider"></li>
-        <?php    
-         endwhile; endif; // done our wordpress loop. Will start again for each category
-    ?>
-        </ul>
-    
-    <?php
-                echo "</div>";
-         } // done the foreach statement 
-    ?>
+        // setup the cateogory ID
+        $cat_id= $cat->term_id;
+        // Make a header for the cateogry
+        echo "<h2>".$cat->name."</h2>";
+        // create a custom wordpress query
+        query_posts("cat=$cat_id&post_per_page=100");
+?>
+    <ul>
+<?php
+        // start the wordpress loop!
+    if (have_posts()) : while (have_posts()) : the_post();
+?>
+        
+        <li><a href="<?php the_permalink();?>"><?php the_title(); ?></a></li>
+<?php    
+    endwhile; endif; // done our wordpress loop. Will start again for each category
+?>
+    </ul>
+
+<?php
+
+    } // done the foreach statement 
+    echo "</div>";
+?>
    
 
 <?php get_footer(); ?>
