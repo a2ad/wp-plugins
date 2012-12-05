c74fa6d5753509c41a1d55764d15d7797791caef
Adriano Ribeiro <adriano.ribeiro@gmail.com>
2012-12-05 18:14:11 -0200

    Alinhando a badget do Github para a esquerda

--- wp-content/themes/wp-plugins_2012/header.php
+++ wp-content/themes/wp-plugins_2012/header.php
@@ -61,7 +61,7 @@
 	</header>
 
   <a href="https://github.com/a2comunicacao/wp-plugins">
-    <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub">
+    <img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png" alt="Fork me on GitHub">
   </a>
 
 <!-- Begin Page -->
